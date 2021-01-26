c8ec8baf2c4f505078064646b5e727ca2c17553e
MeinKraft <saxomonster@hotmail.com>
2021-01-26 22:59:10 +0000

    Upload #1
    Added NastyMobs compatibility
    Added Configs
    Bumped Version

---
+++ src/main/java/crimsonfluff/crimsonshears/ConfigBuilder.java
@@ -0,0 +1,111 @@
+package crimsonfluff.crimsonshears;
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class ConfigBuilder {
+    public final ForgeConfigSpec COMMON;
+
+    public ForgeConfigSpec.BooleanValue ShearGrass;
+    public ForgeConfigSpec.BooleanValue ChickenFeatherDrop;
+    public ForgeConfigSpec.BooleanValue ShearBabies;
+
+    public ForgeConfigSpec.BooleanValue ShearSpider;
+    public ForgeConfigSpec.BooleanValue ShearRabbit;
+    public ForgeConfigSpec.BooleanValue ShearChicken;
+    public ForgeConfigSpec.BooleanValue ShearParrot;
+    public ForgeConfigSpec.BooleanValue ShearGuardian;
+    public ForgeConfigSpec.BooleanValue ShearZombie;
+    public ForgeConfigSpec.BooleanValue ShearHorse;
+    public ForgeConfigSpec.BooleanValue ShearPig;
+    public ForgeConfigSpec.BooleanValue ShearSkeleton;
+    public ForgeConfigSpec.BooleanValue ShearSkeletonHorse;
+    public ForgeConfigSpec.BooleanValue ShearCreeper;
+    public ForgeConfigSpec.BooleanValue ShearZombieHorse;
+    public ForgeConfigSpec.BooleanValue ShearSquid;
+    public ForgeConfigSpec.BooleanValue ShearPiglin;
+    public ForgeConfigSpec.BooleanValue ShearHogZog;
+
+
+    public ConfigBuilder() {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        builder.push("Shears Settings");
+
+
+        ShearGrass = builder
+                .comment("Should Grass Blocks be shearable ?  Default: true")
+                .define("ShearGrass", true);
+
+        ChickenFeatherDrop = builder
+                .comment("Should Chickens randomly drop feathers ?  Default: false")
+                .define("ChickenFeatherDrop", false);
+
+        ShearBabies = builder
+                .comment("Should babies (Chickens/Rabbits/Pigs) be shearable ?  Default: false")
+                .define("ShearBabies", false);
+
+
+        ShearSpider = builder
+                .comment("Should Spiders be shearable ?  Default: true")
+                .define("ShearSpider", true);
+
+        ShearRabbit = builder
+                .comment("Should Rabbits be shearable ?  Default: true")
+                .define("ShearRabbit", true);
+
+        ShearChicken = builder
+                .comment("Should Chickens be shearable ?  Default: true")
+                .define("ShearChicken", true);
+
+        ShearParrot = builder
+                .comment("Should Parrots be shearable ?  Default: true")
+                .define("ShearParrot", true);
+
+        ShearGuardian = builder
+                .comment("Should Guardians be shearable ?  Default: true")
+                .define("ShearGuardian", true);
+
+        ShearZombie = builder
+                .comment("Should Zombies/Husks be shearable ?  Default: true")
+                .define("ShearZombie", true);
+
+        ShearHorse = builder
+                .comment("Should Horses be shearable ?  Default: true")
+                .define("ShearHorse", true);
+
+        ShearPig = builder
+                .comment("Should Pigs be shearable ?  Default: true")
+                .define("ShearPig", true);
+
+        ShearSkeleton = builder
+                .comment("Should Skeletons be shearable ?  Default: true")
+                .define("ShearSkeleton", true);
+
+        ShearSkeletonHorse = builder
+                .comment("Should Skeleton Horses be shearable ?  Default: true")
+                .define("ShearSkeletonHorse", true);
+
+        ShearCreeper = builder
+                .comment("Should Creepers be shearable ?  Default: true")
+                .define("ShearCreeper", true);
+
+        ShearZombieHorse = builder
+                .comment("Should Zombie Horses be shearable ?  Default: true")
+                .define("ShearZombieHorse", true);
+
+        ShearSquid = builder
+                .comment("Should Squid be shearable ?  Default: true")
+                .define("ShearSquid", true);
+
+        ShearPiglin = builder
+                .comment("Should Piglins/Piglin Brutes be shearable ?  Default: true")
+                .define("ShearPiglin", true);
+
+        ShearHogZog = builder
+                .comment("Should Hoglins/Zoglins be shearable ?  Default: true")
+                .define("ShearHogZog", true);
+
+
+        builder.pop();
+
+        COMMON = builder.build();
+    }
+}
--- src/main/java/crimsonfluff/crimsonshears/CrimsonShears.java
+++ src/main/java/crimsonfluff/crimsonshears/CrimsonShears.java
@@ -4,34 +4,32 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.GrassBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.entity.monster.CreeperEntity;
-import net.minecraft.entity.monster.SkeletonEntity;
-import net.minecraft.entity.monster.SpiderEntity;
-import net.minecraft.entity.monster.ZombieEntity;
-import net.minecraft.entity.passive.ChickenEntity;
-import net.minecraft.entity.passive.PigEntity;
-import net.minecraft.entity.passive.RabbitEntity;
-import net.minecraft.entity.passive.horse.HorseEntity;
-import net.minecraft.entity.passive.horse.SkeletonHorseEntity;
-import net.minecraft.entity.passive.horse.ZombieHorseEntity;
+import net.minecraft.entity.monster.*;
+import net.minecraft.entity.monster.piglin.PiglinBruteEntity;
+import net.minecraft.entity.monster.piglin.PiglinEntity;
+import net.minecraft.entity.passive.*;
+import net.minecraft.entity.passive.horse.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.ShearsItem;
-import net.minecraft.particles.IParticleData;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -40,6 +38,7 @@ import org.apache.logging.log4j.Logger;
 public class CrimsonShears {
     public static final String MOD_ID = "crimsonshears";
     public static final Logger LOGGER = LogManager.getLogger("crimsonshears");
+    public static final ConfigBuilder CONFIGURATION = new ConfigBuilder();
     final IEventBus MOD_EVENTBUS = FMLJavaModLoadingContext.get().getModEventBus();
 
     private int dedupe=0;
@@ -48,11 +47,28 @@ public class CrimsonShears {
     public CrimsonShears() {
         dm.setDifficultyScaled();
 
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CONFIGURATION.COMMON);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
+    public void onLivingEntityUpdate(LivingEvent.LivingUpdateEvent event) {
+        if (!CrimsonShears.CONFIGURATION.ChickenFeatherDrop.get()) return;
+
+        if (!event.getEntity().getEntityWorld().isRemote) {
+            Entity living = event.getEntity();
+
+            if (living instanceof ChickenEntity) {
+                // Instead of a random number every tick, drop at same time as egg?
+                if (living.world.rand.nextInt(6000) == 0) living.entityDropItem(new ItemStack(Items.FEATHER));
+            }
+        }
+    }
+
+    @SubscribeEvent
     public void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        if (!CrimsonShears.CONFIGURATION.ShearGrass.get()) return;
+
         World world = event.getWorld();
 
         if (!world.isRemote) {
@@ -94,74 +110,155 @@ public class CrimsonShears {
                 }
 
                 Entity living = event.getTarget();
-                int intDamageAmount=0;
+                int intDamageAmount = 0;
                 Item item = null;
 
-                if (living instanceof SpiderEntity) {
-                    item=Items.STRING;
-                    intDamageAmount=1;
+
+                if (CrimsonShears.CONFIGURATION.ShearSpider.get()) {
+                    if (living instanceof SpiderEntity) {
+                        item = Items.STRING;
+                        intDamageAmount = 4;
+                    }
                 }
 
-                if (living instanceof RabbitEntity) {
-                    item=Items.RABBIT_HIDE;
-                    intDamageAmount=1;
+                if (CrimsonShears.CONFIGURATION.ShearRabbit.get()) {
+                    if (living instanceof RabbitEntity) {
+                        if (((RabbitEntity) living).isChild())
+                            if (!CrimsonShears.CONFIGURATION.ShearBabies.get()) return;
+
+                        item = Items.RABBIT_HIDE;
+                        intDamageAmount = 1;
+                    }
                 }
 
-                if (living instanceof ChickenEntity) {
-                    item=Items.FEATHER;
-                    intDamageAmount=1;
+                if (CrimsonShears.CONFIGURATION.ShearChicken.get()) {
+                    if (living instanceof ChickenEntity) {
+                        if (((ChickenEntity) living).isChild())
+                            if (!CrimsonShears.CONFIGURATION.ShearBabies.get()) return;
+
+                        item = Items.FEATHER;
+                        intDamageAmount = 1;
+                    }
                 }
 
-                if (living instanceof ZombieEntity) {
+                if (CrimsonShears.CONFIGURATION.ShearParrot.get()) {
+                    if (living instanceof ParrotEntity) {
+                        item = Items.FEATHER;
+                        intDamageAmount = 1;
+                    }
+                }
+
+                if (CrimsonShears.CONFIGURATION.ShearGuardian.get()) {
+                    if (living instanceof GuardianEntity) {
+                        item = Items.PRISMARINE_SHARD;
+                        intDamageAmount = 4;
+                    }
+                }
+
+                if (CrimsonShears.CONFIGURATION.ShearZombie.get()) {
+                    if (living instanceof ZombieEntity) {
                     item=Items.ROTTEN_FLESH;
                     intDamageAmount=4;
+                    }
                 }
 
-                if (living instanceof HorseEntity) {
-                    item=Items.LEATHER;
-                    intDamageAmount=6;
+                if (CrimsonShears.CONFIGURATION.ShearHorse.get()) {
+                    if (living instanceof HorseEntity || living instanceof DonkeyEntity || living instanceof MuleEntity) {
+                        item=Items.LEATHER;
+                        intDamageAmount=6;
+                    }
                 }
 
-                if (living instanceof PigEntity) {
-                    item=Items.LEATHER;
-                    intDamageAmount=4;
+                if (CrimsonShears.CONFIGURATION.ShearPiglin.get()) {
+                    if (living instanceof PiglinEntity || living instanceof PiglinBruteEntity) {
+                        item=Items.LEATHER;
+                        intDamageAmount=6;
+                    }
                 }
 
-                if (living instanceof SkeletonEntity) {
-                    item=Items.BONE;
-                    intDamageAmount=4;
+                if (CrimsonShears.CONFIGURATION.ShearPig.get()) {
+                    if (living instanceof PigEntity) {
+                        if (((PigEntity) living).isChild())
+                            if (!CrimsonShears.CONFIGURATION.ShearBabies.get()) return;
+
+                        item=Items.LEATHER;
+                        intDamageAmount=4;
+                    }
                 }
 
-                if (living instanceof CreeperEntity) {
-                    item=Items.GUNPOWDER;
-                    intDamageAmount=4;
+                if (CrimsonShears.CONFIGURATION.ShearHogZog.get()) {
+                    if (living instanceof HoglinEntity) {
+                        item=Items.LEATHER;
+                        intDamageAmount=6;
+                    }
+                    if (living instanceof ZoglinEntity) {
+                        item=Items.ROTTEN_FLESH;
+                        intDamageAmount=6;
+                    }
                 }
 
-                if (living instanceof ZombieHorseEntity) {
-                    item=Items.ROTTEN_FLESH;
-                    intDamageAmount=6;
+                if (CrimsonShears.CONFIGURATION.ShearSkeleton.get()) {
+                    if (living instanceof SkeletonEntity) {
+                        item=Items.BONE;
+                        intDamageAmount=4;
+                    } else {
+
+                        // Add 'NastyMobs' compatibility
+                        if (living.getClass().getSimpleName().equals("NastySkeletonEntity")) {
+                            item=Items.BONE;
+                            intDamageAmount=4;
+                        }
+//                        player.sendStatusMessage(new StringTextComponent("ENTITY: " + living.getClass().getSimpleName()), false);
+//                        player.sendStatusMessage(new StringTextComponent("ENTITY: " + living.getClass().getTypeName()), false);
+                    }
                 }
 
-                if (living instanceof SkeletonHorseEntity) {
-                    item=Items.BONE;
-                    intDamageAmount=6;
+                if (CrimsonShears.CONFIGURATION.ShearCreeper.get()) {
+                    if (living instanceof CreeperEntity) {
+                        item=Items.GUNPOWDER;
+                        intDamageAmount=4;
+                    }
                 }
 
+                if (CrimsonShears.CONFIGURATION.ShearZombieHorse.get()) {
+                    if (living instanceof ZombieHorseEntity) {
+                        item=Items.ROTTEN_FLESH;
+                        intDamageAmount=6;
+                    }
+                }
+
+                if (CrimsonShears.CONFIGURATION.ShearSkeletonHorse.get()) {
+                    if (living instanceof SkeletonHorseEntity) {
+                        item=Items.BONE;
+                        intDamageAmount=6;
+                    }
+                }
+
+                if (CrimsonShears.CONFIGURATION.ShearSquid.get()) {
+                    if (living instanceof SquidEntity) {
+                        item=Items.INK_SAC;
+                        intDamageAmount=2;
+                    }
+                }
 
                 if (intDamageAmount>0) {
                     BlockPos pos = event.getPos();
 
-                    player.swing(player.getActiveHand(), true);
-                    world.playSound(null, pos, SoundEvents.ENTITY_SHEEP_SHEAR, SoundCategory.BLOCKS, 1f, 1f);
+                    // NOTE: If holding carrot in OFF_HAND and use shears, the carrots will animate arm swing -
+                    // can this be stopped?
+                    Hand handIn = (player.getHeldItem(Hand.MAIN_HAND) == player.inventory.getCurrentItem()) ? Hand.MAIN_HAND : Hand.OFF_HAND;
+
+                    player.swing(handIn, true);
+                    world.playSound(null, pos, SoundEvents.ENTITY_SHEEP_SHEAR, SoundCategory.PLAYERS, 1f, 1f);
 
                     // .damageItem checks for Creative mode !
                     player.inventory.getCurrentItem().damageItem(1, player, (playerIn) -> {
-                        playerIn.sendBreakAnimation(player.inventory.player.getActiveHand()); });
+                        playerIn.sendBreakAnimation(handIn); });
 
                     living.attackEntityFrom(dm, intDamageAmount);
                     ((ServerWorld) world).spawnParticle(ParticleTypes.CRIT, living.getPosX(), living.getPosY()+living.getEyeHeight(), living.getPosZ(), 10, 0.5,0.5,0.5,0);
 
-                    world.addEntity(new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(item)));
+                    living.entityDropItem(new ItemStack(item));
 
         // TODO: Stop this event firing Twice  (hence dedupe integer above !)
 
